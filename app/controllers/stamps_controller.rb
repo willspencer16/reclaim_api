6745e986289e3c1a92bbef798b6bfff2470f77b7
Will Spencer <willspencer16@gmail.com>
2021-05-11 15:23:06 +0100

    Added stamps controller

---
+++ app/controllers/stamps_controller.rb
@@ -0,0 +1,29 @@
+class StampsController < ApplicationController
+  def index
+    stamps = Stamp.order("created_at DESC")
+    render json: stamps
+  end
+
+  def create
+    stamp = stamp.create(stamp_param)
+    render json: stamp
+  end
+
+  def update
+    stamp = Stamp.find(params[:id])
+    stamp.update_attributes(stamp_param)
+    render json: stamp
+  end
+
+  def destroy
+    stamp = Stamp.find(params[:id])
+    todo.destroy
+    head :no_content, status: :ok
+  end
+
+  private
+  
+  def stamp_param
+    params.require(:stamp).permit(:user_id, :business_id, :redeemed)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -22,5 +22,4 @@ ActiveRecord::Schema.define(version: 2021_05_11_105513) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
