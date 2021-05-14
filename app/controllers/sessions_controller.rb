ed6c5e9d615d974d2a2c2490fddc5782e169be14
jackmcc08 <jackmcc08@gmail.com>
2021-05-14 11:43:33 +0100

    developed two request tests for sessions create. Need to figure out how to send a 401 response properly in rails

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,16 @@ class SessionsController < ApplicationController
     user = User
       .find_by(username: params["user"]["username"])
       .try(:authenticate, params["user"]["password"])
-      
-      if user
-        session[:user_id] = user.id
-        render json: {
-          status: :created,
-          logged_in: true,
-          user: user
-        }
-      else
-        render json: { status: 401 }
-      end
+
+    if user
+      session[:user_id] = user.id
+      render json: {
+        status: :created,
+        logged_in: true,
+        user: user.username
+      }
+    else
+      render json: { status: 401 }
     end
   end
 end
--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,11 +1,40 @@
+require 'json'
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
   describe "GET /create" do
     it "returns http success" do
-      get "/sessions/create"
-      expect(response).to have_http_status(:success)
+      User.create({ username: "testUser", password:"testPassword"})
+
+      expected_output = {
+        status: :created,
+        logged_in: true,
+        user: "testUser"
+      }
+
+      post "/api/v1/sessions", params: { user: { username: "testUser", password: "testPassword"} }
+
+      # post "/api/v1/sessions"
+
+      expect(response).to be_successful
+      # expect(response).not_to have_http_status(401)
+      expect(response.body).to eq(expected_output.to_json)
+    end
+
+    it 'returns a 401 status if user is not authenticated' do
+      User.create({ username: "testUser", password:"testPassword"})
+
+      expected_output = {
+        status: 401
+      }
+
+      post "/api/v1/sessions", params: { user: { username: "testUser", password: "wrongPassword"} }
+
+      # post "/api/v1/sessions"
+
+      expect(response).to be_successful
+      # expect(response).not_to have_http_status(401)
+      expect(response.body).to eq(expected_output.to_json)
     end
   end
-
 end
