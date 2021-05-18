8c6ad46dc95064a87539213c2a40ef86c3da8bde
Yates101 <alecyates94@gmail.com>
2021-05-18 21:36:41 +0100

    returns user id and username as well as auth token

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,7 +5,8 @@ class AuthenticationController < ApplicationController
     command = AuthenticateUser.call(params[:username], params[:password])
  
     if command.success?
-      render json: { auth_token: command.result }
+      user = User.find_by username: params[:username]
+      render json: { auth_token: command.result, username: user.username, user_id: user.id }
     else
       render json: { error: command.errors }, status: :unauthorized
     end
