7b784ba933513a2dff35ccf313ef8f93a544735d
jackmcc08 <jackmcc08@gmail.com>
2021-05-19 12:58:02 +0100

    trying to console.log

--- app/commands/authorize_api_request.rb
+++ app/commands/authorize_api_request.rb
@@ -23,7 +23,12 @@ class AuthorizeApiRequest
   end
 
   def http_auth_header
+    puts "headers"
     puts headers
+    puts "headers.inspect"
+    puts headers.inspect
+    puts "headers.Authorization"
+    puts headers['Authorization']
     if headers['Authorization'].present?
       return headers['Authorization'].split(' ').last
     else
