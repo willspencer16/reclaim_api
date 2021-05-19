af415729094eda4737f53b9e484b80ae9b6bd6e9
jackmcc08 <jackmcc08@gmail.com>
2021-05-19 13:24:57 +0100

    more puts

--- app/commands/authorize_api_request.rb
+++ app/commands/authorize_api_request.rb
@@ -15,11 +15,19 @@ class AuthorizeApiRequest
 
   def user
     @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
+    puts "boolean - decoded_auth_token"
+    puts decoded_auth_token
+    puts "User"
+    puts @user
     @user || errors.add(:token, 'Invalid token') && nil
+    puts "User - error?"
+    puts @user
   end
 
   def decoded_auth_token
     @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
+    puts "decoded_auth_token"
+    puts @decoded_auth_token
   end
 
   def http_auth_header
