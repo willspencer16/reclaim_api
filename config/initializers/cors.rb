a2515445d29daf9930bc4f569d13cee2547f1c66
jackmcc08 <jackmcc08@gmail.com>
2021-06-08 17:10:25 +0100

    added additional CORS entry point for the hosted frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,4 +15,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
              headers: :any,
              methods: %i[get post put patch delete head]
   end
+
+  allow do
+    origins 'https://reclaim-fe.herokuapp.com'
+
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete head]
+  end
 end
